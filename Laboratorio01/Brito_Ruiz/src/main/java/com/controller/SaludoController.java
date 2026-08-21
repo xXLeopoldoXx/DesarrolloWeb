f1f2f04eb292e0a3747484e14302e30849d874fc
Leopoldo Brito Ruiz <britoleopoldo02@gmail.com>
2026-08-20 21:24:40 -0500

    Corrigiendo ruta del controller

--- Laboratorio01/Brito_Ruiz/src/main/java/com/controller/SaludoController.java
+++ Laboratorio01/Brito_Ruiz/src/main/java/com/controller/SaludoController.java
@@ -1,4 +1,4 @@
-package com.utp.semana1.controller;
+package com.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;

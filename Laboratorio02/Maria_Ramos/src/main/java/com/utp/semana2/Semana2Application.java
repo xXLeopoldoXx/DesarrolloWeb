8a69ea6517c1e180394f5d763b42ed9165bc34fe
mari-div <rmia1929@gmail.com>
2026-08-27 17:03:03 -0500

    Creando lab02

---
+++ Laboratorio02/Maria_Ramos/src/main/java/com/utp/semana2/Semana2Application.java
@@ -0,0 +1,13 @@
+package com.utp.semana2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana2Application.class, args);
+	}
+
+}
---
+++ Laboratorio02/Maria_Ramos/src/main/java/com/utp/semana2/dto/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.utp.semana2.dto;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+    LocalDateTime fecha,
+    int estado,
+    String error,
+    String mensaje,
+    String ruta
+) {}

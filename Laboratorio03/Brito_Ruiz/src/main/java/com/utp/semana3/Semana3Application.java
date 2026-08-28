1fd952582f3f7bad744d2f8050a0ea63e9f5ae10
Leopoldo Brito Ruiz <britoleopoldo02@gmail.com>
2026-08-27 20:45:25 -0500

    Integrando arquitectura
    Desde Spring initializer

---
+++ Laboratorio03/Brito_Ruiz/src/main/java/com/utp/semana3/Semana3Application.java
@@ -0,0 +1,13 @@
+package com.utp.semana3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana3Application.class, args);
+	}
+
+}
---
+++ Laboratorio03/Brito_Ruiz/src/test/java/com/utp/semana3/Semana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.semana3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

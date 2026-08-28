fe0071d76a32d583cca2b7c9de3fbb91d28e0f36
Anelhi Marcos <u22220212@utp.edu.pe>
2026-08-27 21:29:48 -0500

    Completando controlador y pruebas MockMvc - Laboratorio 3

---
+++ Laboratorio03/Anelhi_Marcos/src/main/java/com/utp/semana3/controller/ProductoController.java
@@ -0,0 +1,36 @@
+﻿package com.utp.semana3.controller;
+
+import com.utp.semana3.model.Producto;
+import com.utp.semana3.service.ProductoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping
+    public List<Producto> listar() {
+        return productoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> buscarPorId(@PathVariable Long id) {
+        return productoService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Producto registrar(@RequestBody Producto producto) {
+        return productoService.registrar(producto);
+    }
+}

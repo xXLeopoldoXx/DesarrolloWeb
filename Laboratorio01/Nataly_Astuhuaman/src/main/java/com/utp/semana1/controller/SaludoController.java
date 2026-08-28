7c64dcc89d56feb6f45d9f88b2a36be1ecddbfee
JosephEquisde <josephpajuelo886@gmail.com>
2026-08-27 22:11:29 -0500

    feat: implementacion completa Guia 1 - Nataly Astuhuaman

---
+++ Laboratorio01/Nataly_Astuhuaman/src/main/java/com/utp/semana1/controller/SaludoController.java
@@ -0,0 +1,37 @@
+package com.utp.semana1.controller;
+
+import com.utp.semana1.model.EstadoInfo;
+import com.utp.semana1.model.ProyectoInfo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SaludoController {
+
+    @GetMapping("/saludo")
+    public String saludar() {
+        return "Hola, Spring Boot está funcionando correctamente";
+    }
+
+    @GetMapping("/info")
+    public ProyectoInfo obtenerInfo() {
+        return new ProyectoInfo(
+                "Desarrollo Web Integrado",
+                "Semana 1",
+                "Spring Boot + Maven + Java 25",
+                "Entorno configurado correctamente"
+        );
+    }
+
+    @GetMapping("/version")
+    public String obtenerVersion() {
+        return "Aplicación Semana 1 Versión 1.0.0";
+    }
+
+    @GetMapping("/estado")
+    public EstadoInfo obtenerEstado() {
+        return new EstadoInfo("semana1", true, "Backend disponible");
+    }
+}

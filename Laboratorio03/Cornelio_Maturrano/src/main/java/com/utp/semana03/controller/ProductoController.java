3a4cb20073c66e2110f0fb1d75481ad59cc39f23
mari-div <rmia1929@gmail.com>
2026-09-03 20:49:45 -0500

    actualizar

---
+++ Laboratorio03/Cornelio_Maturrano/src/main/java/com/utp/semana03/controller/ProductoController.java
@@ -0,0 +1,25 @@
+package com.utp.semana03.controller;
+
+import com.utp.semana03.model.Producto;
+import com.utp.semana03.service.ProductoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping
+    public List<Producto> listar() {
+        return productoService.listar();
+    }
+}
---
+++ Laboratorio03/Cornelio_Maturrano/src/main/java/com/utp/semana03/model/Producto.java
@@ -0,0 +1,41 @@
+package com.utp.semana03.model;
+public class Producto {
+ private Long id;
+ private String nombre;
+ private double precio;
+ private int stock;
+
+ public Producto() {
+ }
+ 
+ public Producto(Long id, String nombre, double precio, int stock) {
+ this.id = id;
+ this.nombre = nombre;
+ this.precio = precio;
+ this.stock = stock;
+ }
+ public Long getId() {
+ return id;
+ }
+ public void setId(Long id) {
+ this.id = id;
+ }
+ public String getNombre() {
+ return nombre;
+ }
+ public void setNombre(String nombre) {
+ this.nombre = nombre;
+ }
+ public double getPrecio() {
+ return precio;
+ }
+ public void setPrecio(double precio) {
+ this.precio = precio;
+ }
+ public int getStock() {
+ return stock;
+ }
+ public void setStock(int stock) {
+ this.stock = stock;
+ }
+}

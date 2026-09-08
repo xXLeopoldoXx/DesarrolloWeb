10eaafbae6b1746784efd5de984a434145332bbd
Leopoldo Brito Ruiz <britoleopoldo02@gmail.com>
2026-09-07 23:12:30 -0500

    Subiendo arquitectura spring

---
+++ src/main/java/com/utp/proyecto_riesgo/ProyectoRiesgoApplication.java
@@ -0,0 +1,13 @@
+package com.utp.proyecto_riesgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRiesgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRiesgoApplication.class, args);
+	}
+
+}

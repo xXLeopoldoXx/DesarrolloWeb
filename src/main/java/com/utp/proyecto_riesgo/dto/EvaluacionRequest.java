be90486ba610cf719800926bae055ceca488dd47
Anelhi Marcos <u22220212@utp.edu.pe>
2026-09-08 20:37:29 -0500

    agregndo lógica y validaciones a Evaluacion y EvaluacionRequest

---
+++ src/main/java/com/utp/proyecto_riesgo/dto/EvaluacionRequest.java
@@ -0,0 +1,29 @@
+package com.utp.proyecto_riesgo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record EvaluacionRequest(
+        @NotBlank(message = "El seudónimo de la víctima es obligatorio")
+        String seudonimoVictima,
+        
+        @NotNull(message = "La edad es obligatoria")
+        @Positive(message = "La edad debe ser un número positivo")
+        Integer edadVictima,
+        
+        @NotBlank(message = "La relación con el agresor no puede estar vacía")
+        String relacionAgresor,
+        
+        boolean amenazaMuerte,
+        boolean violenciaFisicaPrevia,
+        boolean estrangulamientoPrevio,
+        boolean accesoArmas,
+        boolean incumplimientoMedidaProteccion,
+        boolean acosoOPersecucion,
+        boolean separacionReciente,
+        boolean controlExtremo,
+        boolean consumoAlcoholDrogasAgresor,
+        boolean denunciasPrevias,
+        boolean hijosEnComun
+) {}
